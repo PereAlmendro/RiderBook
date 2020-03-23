843c63ee0b71dd7260d9efd950535665eb750586
Pere Almendro <pereab@ext.bershka.com>
2020-03-23 14:40:30 +0100

    reorganised code

---
+++ RiderBook/RiderBook/DI/ViewModule.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModule.swift
+//  NewBershka
+//
+//  Created by Pere Almendro on 22/03/2020.
+//  Copyright © 2020 Pere Almendro. All rights reserved.
+//
+
+import Foundation
+
+public class ViewModule {
+    
+}
