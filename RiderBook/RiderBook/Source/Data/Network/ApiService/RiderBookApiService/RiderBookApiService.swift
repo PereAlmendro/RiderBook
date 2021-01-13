3c825d67a0cf337c4a85ab6286234cd8ae6a40dd
Pere Almendro <palmendrob@gmail.com>
2021-01-13 10:01:46 +0100

    remove unnecessary maps and indent code

--- RiderBook/RiderBook/Source/Data/Network/ApiService/RiderBookApiService/RiderBookApiService.swift
+++ RiderBook/RiderBook/Source/Data/Network/ApiService/RiderBookApiService/RiderBookApiService.swift
@@ -12,62 +12,62 @@ import Combine
 protocol RiderBookApiServiceProtocol {
     func loadRequest<ResponseModel: Decodable>(_ target: ApiTargetProtocol,
                                                responseModel: ResponseModel.Type)
-        -> AnyPublisher<ResponseModel?, RiderBookError>
+    -> AnyPublisher<ResponseModel?, RiderBookError>
 }
 
 final class RiderBookApiService: RiderBookApiServiceProtocol {
     func loadRequest<ResponseModel: Decodable>(_ target: ApiTargetProtocol,
                                                responseModel: ResponseModel.Type)
-        -> AnyPublisher<ResponseModel?, RiderBookError> {
-            
-            guard let url = URL(string: target.baseUrl),
-                var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
-                    return getbadUrlError(responseModel: responseModel)
-            }
-            
-            urlComponents.path = urlComponents.path.appending(target.endPoint)
-            urlComponents.queryItems = target.queryItems
-            
-            guard let finalUrl = urlComponents.url else {
-                return getbadUrlError(responseModel: responseModel)
-            }
-            var request = URLRequest(url: finalUrl)
-            
-            request.httpMethod = target.method.rawValue
-            request.allHTTPHeaderFields = target.headers
-            
-            if let requestJsonData = target.requestObject?.toJsonData() {
-                request.httpBody = requestJsonData
-            }
-            
-            let decoder = JSONDecoder()
-            
-            #if DEBUG
-            print("\nREQUEST : ")
-            print(request.url?.absoluteString ?? "Empty URL")
-            #endif
-            
-            return URLSession.shared.dataTaskPublisher(for: request)
-                .map { data, response in data }
-                .mapError { error in RiderBookError.responseError(error) }
-                .decode(type: responseModel, decoder: decoder)
-                .mapError { error in RiderBookError.responseError(error) }
-                .map({ (result) -> ResponseModel? in
-                    #if DEBUG
-                    print("\nRESPONSE : ")
-                    print(request.url?.absoluteString ?? "Empty URL")
-                    print(result)
-                    #endif
-                    return result
-                })
-                .eraseToAnyPublisher()
+    -> AnyPublisher<ResponseModel?, RiderBookError> {
+
+        guard
+            let url = URL(string: target.baseUrl),
+            var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+            return getbadUrlError(responseModel: responseModel)
+        }
+
+        urlComponents.path = urlComponents.path.appending(target.endPoint)
+        urlComponents.queryItems = target.queryItems
+
+        guard let finalUrl = urlComponents.url else {
+            return getbadUrlError(responseModel: responseModel)
+        }
+        var request = URLRequest(url: finalUrl)
+
+        request.httpMethod = target.method.rawValue
+        request.allHTTPHeaderFields = target.headers
+
+        if let requestJsonData = target.requestObject?.toJsonData() {
+            request.httpBody = requestJsonData
+        }
+
+        let decoder = JSONDecoder()
+
+        #if DEBUG
+        print("\nREQUEST : ")
+        print(request.url?.absoluteString ?? "Empty URL")
+        #endif
+
+        return URLSession.shared.dataTaskPublisher(for: request)
+            .map { data, response in data }
+            .mapError { error in RiderBookError.responseError(error) }
+            .decode(type: responseModel, decoder: decoder)
+            .mapError { error in RiderBookError.responseError(error) }
+            .map({ (result) -> ResponseModel? in
+                #if DEBUG
+                print("\nRESPONSE : ")
+                print(request.url?.absoluteString ?? "Empty URL")
+                print(result)
+                #endif
+                return result
+            })
+            .eraseToAnyPublisher()
     }
     
     private func getbadUrlError<ResponseModel: Decodable>(responseModel: ResponseModel.Type)
-        -> AnyPublisher<ResponseModel?, RiderBookError> {
-        return Result<Int, Error>.Publisher(URLError(.badURL))
-            .map({ response -> ResponseModel? in nil })
-            .mapError({ error in RiderBookError.badRequest(error) })
+    -> AnyPublisher<ResponseModel?, RiderBookError> {
+        let badUrlError = RiderBookError.badRequest(URLError(.badURL))
+        return Result<ResponseModel?, RiderBookError>.Publisher(badUrlError)
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
